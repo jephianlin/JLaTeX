86b1486173df07fd1d642d3b24a1dda1a7938606
Jephian Lin <jephianlin@gmail.com>
2026-03-08 20:57:18 +0800

    update CV to two versions

---
+++ makecv/all-cv.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+head -n 3 active-CV.tex > active-CV-long.tex
+tail -n +7 active-CV.tex >> active-CV-long.tex
+
+head -n 6 active-CV.tex > active-CV-short.tex
+tail -n +10 active-CV.tex >> active-CV-short.tex
+
+pdflatex active-CV-long.tex; bibtex active-CV-long.1.aux; bibtex active-CV-long.2.aux; bibtex active-CV-long.3.aux; bibtex active-CV-long.4.aux; bibtex active-CV-long.5.aux; bibtex active-CV-long.6.aux; bibtex active-CV-long.7.aux; pdflatex -synctex=1 -interact=nonstopmode 'active-CV-long.tex'
+
+pdflatex active-CV-short.tex; bibtex active-CV-short.1.aux; bibtex active-CV-short.2.aux; bibtex active-CV-short.3.aux; bibtex active-CV-short.4.aux; bibtex active-CV-short.5.aux; bibtex active-CV-short.6.aux; bibtex active-CV-short.7.aux; pdflatex -synctex=1 -interact=nonstopmode 'active-CV-short.tex'
