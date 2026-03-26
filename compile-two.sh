abd17f2e58e5d1ae4e7ac8ae39aacc6f3d7d3e65
Jephian Lin <jephianlin@gmail.com>
2026-03-26 14:01:52 +0800

    add exam-template

---
+++ compile-two.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+FILENAME="${1##*/}"
+BASENAME="${FILENAME%.*}"
+
+head -n 3 $FILENAME > $BASENAME-A.tex
+tail -n +7 $FILENAME >> $BASENAME-A.tex
+
+head -n 6 $FILENAME > $BASENAME-B.tex
+tail -n +10 $FILENAME >> $BASENAME-B.tex
+
+# pdflatex $BASENAME-A.tex
+
+# pdflatex $BASENAME-B.tex
